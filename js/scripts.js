c2c3a9bf87b33cfff3b9d6d18a81b97cf53b14de
Patrick Dolan <dolanp1992@gmail.com>
2021-11-19 16:59:22 -0800

    add ability for greetings() function to recognize 1, 2, and 3 and replace them with proper response

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,16 @@ function greetings(number) {
   for (let i = 0; i <= number; i++) {
     userNumber.push(i);
   }
-  return userNumber;
+  const roboReply = userNumber.map(function(element) {
+    if (element === 3) {
+      return "Will you be my neighbor?";
+    } else if (element === 2) {
+      return "Boop!";
+    } else if (element === 1) {
+      return "Beep!"
+    } else {
+      return element;
+    }
+  });
+  return roboReply;
 }
